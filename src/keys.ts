d95033ddb93f26a68c48f39338c03727a22d2b56
Manuel Valls <me@manvalls.com>
2023-04-09 11:51:13 +0200

    Move from Symbols to strings

--- src/keys.ts
+++ src/keys.ts
@@ -1,4 +1,4 @@
 export const versionKey = '__GOSEN_PAGE_VERSION__'
 export const stateKey = '__GOSEN_PAGE_STATE__'
-export const entryUID = Symbol('entryUID')
-export const lastAppliedURL = Symbol('lastAppliedURL')
+export const entryUID = '__GOSEN_PAGE_ENTRY_UID__'
+export const lastAppliedURL = '__GOSEN_PAGE_LAST_APPLIED_URL__'
